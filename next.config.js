e786601d63b3d7485d6f57a9c0794d70ce6b261a
gerson1232 <42564125+gerson1232@users.noreply.github.com>
2021-06-11 18:27:34 -0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        "BASE_URL": "http://localhost:3000",
+        "MONGODB_URL": "mongodb+srv://emergentes:emergentes@cluster0.b6hpr.mongodb.net/te_proyecto_ferreteria?retryWrites=true&w=majority",
+        "ACCESS_TOKEN_SECRET": "ek%LcLD3TL#dSA~*4X'+X_a?j$dfM*@VS2Ler}5j6^+N4rX+p?",
+        "REFRESH_TOKEN_SECRET": "!@wsQBvS$9^YkvptLk)mGEq;}#H~Efd,[^yj45NNWaJt3}B7:D'+J.S9Rk#~h-gpf8{V4%[Y?5T:hT~",
+        "PAYPAL_CLIENT_ID": "AdTjrk4vgvxveP7MQ0OBlmoVCg5JACeLAXZQcb2pf3PBCw6_8hdOrauB6JQoauHaV3X7Ibo00U3n5xeE",
+        "CLOUD_UPDATE_PRESET": "te_proyecto_ferreteria",
+        "CLOUD_NAME": "uu",
+        "CLOUD_API": "https://api.cloudinary.com/v1_1/uu/image/upload"
+    }
+}
